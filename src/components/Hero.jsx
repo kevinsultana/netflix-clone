7b731f10d5381f9d35f90914080a50d4b53ef9f1
kevinsultana <kevinpdg.ks@gmail.com>
2025-06-18 00:58:18 +0700

    feat: added hero section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import hero from "../assets/hero/hero.png";
+import logoGlitch from "../assets/logoGlitch.png";
+import { FaPlay } from "react-icons/fa6";
+import { IoIosInformationCircleOutline } from "react-icons/io";
+
+export default function Hero() {
+  return (
+    <div>
+      <div>
+        <img src={hero} alt="Hero" className="w-full h-screen absolute z-0" />
+        <div class="absolute inset-0 bg-gradient-to-b from-black to-transparent opacity-20"></div>
+      </div>
+      <div className="absolute z-10 px-14 mt-56 max-w-2xl">
+        <div className="flex gap-4 items-center">
+          <img src={logoGlitch} alt="logo-glitch" className="w-20" />
+          <p className=" text-white uppercase font-bold text-2xl">
+            s e r i e s
+          </p>
+        </div>
+        <h1 className="text-white text-7xl font-bold uppercase">
+          House of ninjas
+        </h1>
+        <p className="text-white my-6 text-lg">
+          Years after retiring from their formidable ninja lives, a
+          dysfunctional family must return to shadowy missions to counteract a
+          string of looming threats.
+        </p>
+        <div className="flex gap-4">
+          <button className="bg-white text-black py-2 px-8 flex gap-2 items-center rounded-xl">
+            <FaPlay />
+            <span>Play</span>
+          </button>
+          <button className="bg-white/20 text-white py-2 px-8 flex gap-2 items-center rounded-xl">
+            <IoIosInformationCircleOutline />
+            <span>More Info</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,9 @@
-import { FaMagnifyingGlass, FaChevronDown, FaRegBell } from "react-icons/fa6";
+import {
+  FaMagnifyingGlass,
+  FaChevronDown,
+  FaRegBell,
+  FaRegHeart,
+} from "react-icons/fa6";
 import logo from "../assets/logo.png";
 import ava1 from "../assets/avatar/ava1.png";
 
@@ -27,23 +32,26 @@ export default function NavBar() {
   ];
 
   return (
-    <div className="flex justify-between px-14 py-4 items-center fixed top-0 left-0 right-0 ">
+    <div className="flex justify-between px-14 py-4 items-center fixed top-0 left-0 right-0 z-10 ">
       <div className="flex gap-12 items-center">
         <img src={logo} alt="logo" className="w-24 object-contain" />
         <nav>
           <ul className="flex gap-4">
             {dataNavBar.map((item) => (
-              <li key={item.id}>{item.name}</li>
+              <li key={item.id} className="text-white">
+                {item.name}
+              </li>
             ))}
           </ul>
         </nav>
       </div>
       <div className="flex gap-6 items-center">
-        <FaMagnifyingGlass className="text-2xl text-black" />
-        <FaRegBell className="text-2xl text-black" />
+        <FaMagnifyingGlass className="text-2xl text-white" />
+        {/* <FaRegBell className="text-2xl text-white" /> */}
+        <FaRegHeart className="text-2xl text-white" />
         <button className="flex items-center gap-2">
           <img src={ava1} alt="avatar" className="w-8" />
-          <FaChevronDown />
+          <FaChevronDown className="text-lg text-white" />
         </button>
       </div>
     </div>

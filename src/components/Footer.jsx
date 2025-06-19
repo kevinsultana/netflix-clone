bd1467234089a6ce64c88570a74fbfdaeb7dfced
kevinsultana <kevinpdg.ks@gmail.com>
2025-06-20 01:21:46 +0700

    style: update responsive phase 3

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -18,25 +18,25 @@ export default function Footer() {
         <FaYoutube className={iconClassName} />
       </div>
       <div className="text-white/60 text-xs md:text-lg mb-8">
-        <div className="grid grid-cols-4 md:gap-40 pb-4">
+        <div className="grid grid-cols-4 gap-4 md:gap-40 pb-4">
           <p>Audio Description</p>
           <p>Help Center</p>
           <p>Gift Card</p>
           <p>Media Center</p>
         </div>
-        <div className="grid grid-cols-4 md:gap-40 pb-4">
+        <div className="grid grid-cols-4 gap-4 md:gap-40 pb-4">
           <p>Investor Relations</p>
           <p>Legal Notices</p>
           <p>Netvlix Shops</p>
           <p>Term of Use</p>
         </div>
-        <div className="grid grid-cols-4 md:gap-40 pb-4">
+        <div className="grid grid-cols-4 gap-4 md:gap-40 pb-4">
           <p>Privacy</p>
           <p>Legal Notices</p>
           <p>Cookies Preferences</p>
           <p>Corporate Information</p>
         </div>
-        <div className="grid grid-cols-4 md:gap-40 pb-4">
+        <div className="grid grid-cols-4 gap-4 md:gap-40 pb-4">
           <p>Contact Us</p>
           <p>Do Not Sell or Share My Personal Information</p>
           <p>Ad Choices</p>
@@ -49,7 +49,7 @@ export default function Footer() {
       </div>
       <div className="flex justify-center">
         <div className="text-white/60 mt-8 text-xs md:text-lg">
-          <p>© 2025 Netflix, Inc. All rights reserved.</p>
+          <p>© 2025 Netvlix, Inc. All rights reserved.</p>
         </div>
       </div>
     </div>
